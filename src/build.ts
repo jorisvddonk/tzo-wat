518d55c8930e0fc52eae3bbccf9d58523c85754c
joris <jorisvddonk@gmail.com>
2020-10-11 22:01:00 +0200

    remove old implementation and usage of node-sexp

--- src/build.ts
+++ src/build.ts
@@ -1,50 +1,8 @@
 import { Instruction } from "tzo";
-import sexp from "node-sexp";
 import 'array-flat-polyfill';
 import { Analyzer } from "tzo-analyze";
 import { Expression } from "tzo-analyze/dist/interfaces";
 import binaryen from "binaryen";
-
-const genFunction = (fname) => {
-  return (...args: any[]) => {
-    return sexp(fname, args);
-  }
-};
-
-const genStr = (sname) => {
-  return (...args: any[]) => {
-    return `${sname} ${args.join(" ")}`;
-  }
-}
-
-const str = (val) => {
-  return `"${val.replace(/\"/i, '\"')}"`
-}
-
-const func = genFunction('func');
-const param = genFunction('param');
-const result = genFunction('result');
-const exprt = genFunction('export');
-const imprt = genFunction('import');
-const call = genStr('call');
-const nop = genStr('nop');
-const memory = genFunction('memory');
-const data = genFunction('data');
-const local: any = genFunction('local');
-(local as any).get = genStr('get_local');
-(local as any).set = genStr('set_local');
-
-const i32 = {
-  add: genStr("i32.add"),
-  load: genStr("i32.load"),
-  store: genStr("i32.store"),
-  const: genStr("i32.const"),
-  eq: genStr("i32.eq"),
-  ne: genStr("i32.ne"),
-  or: genStr("i32.or"),
-  and: genStr("i32.and"),
-}
-i32.toString = () => "i32";
 
 interface StringTable {
   [key: string]: number
@@ -56,7 +14,6 @@ export class Builder {
   imports: string[] = [];
   input: Instruction[] = undefined;
   analysis: WasmExpression[] = [];
-  wasm_module = sexp("module");
   stringtable: StringTable = {};
   strtbl_i = 0;
   module = new binaryen.Module();
@@ -147,31 +104,6 @@ export class Builder {
     return retval;
   }
 
-  expressionToString(expression: WasmExpression) {
-    if (expression.type === "block" && expression.value === "if") {
-      return `${this.treeToString(expression.conditionBody)}
-if $I0 ${expression.produces > 0 ? `(result i32)` : ``}
-${this.treeToString(expression.thenBody)}
-end`;
-    }
-    if (expression.type === "number_literal") {
-      return this.convertInstruction({ type: "push-number-instruction", value: expression.value });
-    }
-    if (expression.type === "string_literal") {
-      return this.convertInstruction({ type: "push-string-instruction", value: expression.value });
-    }
-    if (expression.type === "function" && expression.value === "goto") {
-      // skip gotos!
-      this.unknownInstructions["goto"] = (this.unknownInstructions["goto"] === undefined ? 0 : this.unknownInstructions["goto"]) + 1;
-      return ``;
-    }
-    return `${this.treeToString(expression.children as WasmExpression[])}
-${(this.convertInstruction({
-      type: "invoke-function-instruction",
-      functionName: expression.value
-    }) || "").toString()}`;
-  }
-
   expressionToBinaryen: (expression: WasmExpression) => number = (expression: WasmExpression) => {
     function assertLength(children: (Expression | WasmExpression)[], maxLength: number, helpText?: string) {
       if (children.length > maxLength) {
@@ -246,10 +178,6 @@ ${(this.convertInstruction({
     throw new Error(`Unimplemented Tzo parsed code type/value: ${expression.type} // ${expression.value}`);
   }
 
-  treeToString(expressions: WasmExpression[]) {
-    return expressions.map(x => this.expressionToString(x)).join('\n');
-  }
-
   treeToBinaryen(expressions: WasmExpression[]) {
     return expressions.map(x => this.expressionToBinaryen(x));
   }
@@ -277,87 +205,6 @@ ${(this.convertInstruction({
       params: b_params,
       result: b_results
     }
-    this.wasm_module.nodes.push(
-      func(`$${importName}`, imprt(str("imports"), str(importName)), ...params, ...results)
-    )
-  }
-
-  convertInstruction(i: Instruction) {
-    function c(instr: (Instruction | string)[]) {
-      return instr.map(x => x.toString()).join('\n');
-    }
-
-    if (i.type === "push-string-instruction") {
-      return i32.const(this.stringtable[i.value]) + ` ;; ${i.value}`;
-    }
-    if (i.type === "push-number-instruction") {
-      return i32.const(i.value);
-    }
-    if (i.type === "invoke-function-instruction" && i.functionName === "getContext") {
-      return i32.load();
-    }
-    if (i.type === "invoke-function-instruction" && i.functionName === "setContext") {
-      return i32.store("align=2");
-    }
-    if (i.type === "invoke-function-instruction" && i.functionName === "or") {
-      return c([
-        i32.const(0),
-        i32.ne(),
-        local.set("$sA"),
-        i32.const(0),
-        i32.ne(),
-        local.get("$sA"),
-        i32.or()
-      ]);
-    }
-    if (i.type === "invoke-function-instruction" && i.functionName === "and") {
-      return c([
-        i32.const(0),
-        i32.ne(),
-        local.set("$sA"),
-        i32.const(0),
-        i32.ne(),
-        local.get("$sA"),
-        i32.and()
-      ]);
-    }
-    if (i.functionName === "not") {
-      return c([
-        i32.const(1),
-        i32.eq(),
-        `if $I0 (result i32)`,
-        i32.const(0),
-        `else`,
-        i32.const(1),
-        `end`
-      ])
-    }
-    if (i.functionName === "+" || i.functionName === "plus") {
-      return i32.add();
-    }
-    if (i.functionName === "nop") {
-      return nop();
-    }
-    if (i.functionName === "eq") {
-      return i32.eq();
-    }
-    if (i.functionName === "pause") {
-      return call('$pause');
-    }
-
-    if (this.imports.includes(i.functionName)) {
-      return call(`$${i.functionName}`);
-    }
-
-
-    this.unknownInstructions[i.functionName] = (this.unknownInstructions[i.functionName] === undefined ? 0 : this.unknownInstructions[i.functionName]) + 1;
-
-  }
-
-  getDatas() {
-    return Object.entries(this.stringtable).map(entry => {
-      return data(`(${i32.const(entry[1])})`, str(entry[0] + "\\00"));
-    });
   }
 
   getDatas_binaryen() {
@@ -371,11 +218,6 @@ ${(this.convertInstruction({
   }
 
   build() {
-    const neededLocals = [
-      local("$sA", i32),
-      local("$sB", i32)
-    ]
-
     this.module.setMemory(1, 1, "pagememory", this.getDatas_binaryen());
 
     this.module.addFunction("main", binaryen.createType([]), binaryen.none, [binaryen.i32, binaryen.i32], this.module.block(null,
@@ -383,21 +225,9 @@ ${(this.convertInstruction({
     ));
     this.module.addFunctionExport("main", "main");
 
-    this.wasm_module.nodes.push(
-      // imprt(str("js"), str("mem"), memory(1)),
-      `(memory $0 1)`,
-      ...this.getDatas(),
-      `(export "pagememory" (memory $0))`,
-      func(
-        "$main", ...neededLocals, '\n', this.treeToString(this.analysis)
-      ),
-      exprt(str("main"), func("$main"))
-    )
-
     console.warn(`Unknown instructions: ${JSON.stringify(this.unknownInstructions, null, 2)}`);
 
     return this.module.emitText();
-    //return this.wasm_module;
   }
 
 }
